725213f330d9d429d09f46335aad63c2078543b0
Juan <pabloah09@gmail.com>
2025-03-27 13:29:59 -0300

    feat: melhorias na interface do usuário e validação de tarefas duplicadas

--- to-do-list/obj/Debug/net8.0/to-do-list.AssemblyInfo.cs
+++ to-do-list/obj/Debug/net8.0/to-do-list.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("to-do-list")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59ee76c86aa58a76fd469555ae417640eeb86249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab04cf1f0933077d6db5fd58609cfacb2d952df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("to-do-list")]
 [assembly: System.Reflection.AssemblyTitleAttribute("to-do-list")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
